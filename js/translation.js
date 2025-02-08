8db6bf0c8e179f8569b5d37b0363b748c6ec143b
miguelable <miguelff222@gmail.com>
2025-02-08 16:37:21 +0100

    Actualizar enlaces de navegación y eliminar traducciones de idiomas no utilizadas

--- js/translation.js
+++ js/translation.js
@@ -2,21 +2,18 @@ const translations = {
   es: {
     home: "Inicio",
     link: "Enlace",
-    idiomas: "Idiomas",
     search: "Buscar",
     masterTeleco: "Master Teleco",
   },
   en: {
     home: "Home",
     link: "Link",
-    idiomas: "Languages",
     search: "Search",
     masterTeleco: "Master Teleco",
   },
   fr: {
     home: "Accueil",
     link: "Lien",
-    idiomas: "Langues",
     search: "Rechercher",
     masterTeleco: "Master Teleco",
   },
@@ -29,8 +26,6 @@ function changeLanguage(lang) {
     translations[lang].home;
   document.querySelector('.nav-link[href="#"]').textContent =
     translations[lang].link;
-  document.querySelector(".dropdown-toggle").textContent =
-    translations[lang].idiomas;
   document.querySelector(".form-control").placeholder =
     translations[lang].search;
 }
