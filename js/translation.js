a8fd23ae1cb8403fffe485e717cf57c65d4c4c4c
miguelable <miguelff222@gmail.com>
2025-02-08 17:20:25 +0100

    Agregar identificadores a elementos de navegación y formularios en HTML

--- js/translation.js
+++ js/translation.js
@@ -1,46 +1,66 @@
 const translations = {
   es: {
-    home: "Inicio",
-    link: "Enlace",
-    search: "Buscar",
-    masterTeleco: "Master Teleco",
     light_text: "Claro",
     dark_text: "Oscuro",
     auto_text: "Auto",
+    home_text: "Inicio",
+    admin_text: "Administración",
+    lenguage_text: "Idioma",
+    spanish_text: "Español",
+    english_text: "Inglés",
+    french_text: "Francés",
+    search_text: "Buscar",
   },
   en: {
-    home: "Home",
-    link: "Link",
-    search: "Search",
-    masterTeleco: "Master Teleco",
     light_text: "Light",
     dark_text: "Dark",
     auto_text: "Auto",
+    home_text: "Home",
+    admin_text: "Administration",
+    lenguage_text: "Language",
+    spanish_text: "Spanish",
+    english_text: "English",
+    french_text: "French",
+    search_text: "Search",
   },
   fr: {
-    home: "Accueil",
-    link: "Lien",
-    search: "Rechercher",
-    masterTeleco: "Master Teleco",
     light_text: "Lumière",
     dark_text: "Sombre",
     auto_text: "Auto",
+    home_text: "Accueil",
+    admin_text: "Administration",
+    lenguage_text: "Langue",
+    spanish_text: "Espagnol",
+    english_text: "Anglais",
+    french_text: "Français",
+    search_text: "Rechercher",
   },
 };
 
 function changeLanguage(lang) {
-  document.querySelector(".navbar-brand").textContent =
-    translations[lang].masterTeleco;
-  document.querySelector(".nav-link.active").textContent =
-    translations[lang].home;
-  document.querySelector('.nav-link[href="#"]').textContent =
-    translations[lang].link;
-  document.querySelector(".form-control").placeholder =
-    translations[lang].search;
+  // Cambiar el texto de los elementos de luz, oscuro y auto
   document.getElementById("light_text").textContent =
     translations[lang].light_text;
   document.getElementById("dark_text").textContent =
     translations[lang].dark_text;
   document.getElementById("auto_text").textContent =
     translations[lang].auto_text;
+
+  // Cambiar el texto de los enlaces de la barra de navegación
+  document.getElementById("home_text").textContent =
+    translations[lang].home_text;
+  document.getElementById("admin_text").textContent =
+    translations[lang].admin_text;
+  document.getElementById("language_text").textContent =
+    translations[lang].lenguage_text;
+  document.getElementById("spanish_text").textContent =
+    translations[lang].spanish_text;
+  document.getElementById("english_text").textContent =
+    translations[lang].english_text;
+  document.getElementById("french_text").textContent =
+    translations[lang].french_text;
+  document.getElementById("search_text").textContent =
+    translations[lang].search_text;
+  document.getElementById("search_input").placeholder =
+    translations[lang].search_text;
 }
