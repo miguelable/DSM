5dc945a9aada310b58a0f987b31c17dbe2614191
miguelable <miguelff222@gmail.com>
2025-02-08 16:56:56 +0100

    Agregar texto para los modos de visualización claro, oscuro y automático en el HTML y actualizar estilos

--- js/translation.js
+++ js/translation.js
@@ -4,18 +4,27 @@ const translations = {
     link: "Enlace",
     search: "Buscar",
     masterTeleco: "Master Teleco",
+    light_text: "Claro",
+    dark_text: "Oscuro",
+    auto_text: "Auto",
   },
   en: {
     home: "Home",
     link: "Link",
     search: "Search",
     masterTeleco: "Master Teleco",
+    light_text: "Light",
+    dark_text: "Dark",
+    auto_text: "Auto",
   },
   fr: {
     home: "Accueil",
     link: "Lien",
     search: "Rechercher",
     masterTeleco: "Master Teleco",
+    light_text: "Lumière",
+    dark_text: "Sombre",
+    auto_text: "Auto",
   },
 };
 
@@ -28,4 +37,10 @@ function changeLanguage(lang) {
     translations[lang].link;
   document.querySelector(".form-control").placeholder =
     translations[lang].search;
+  document.getElementById("light_text").textContent =
+    translations[lang].light_text;
+  document.getElementById("dark_text").textContent =
+    translations[lang].dark_text;
+  document.getElementById("auto_text").textContent =
+    translations[lang].auto_text;
 }
