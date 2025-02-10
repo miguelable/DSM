38bd6879c45a82c8d1630d7370b5a396f95dc99d
miguelable <miguelff222@gmail.com>
2025-02-10 18:45:58 +0100

    Agregar validación de correo electrónico y contraseña en el formulario, con mensajes de error específicos

--- js/form.js
+++ js/form.js
@@ -8,13 +8,33 @@ document
       form.classList.add("was-validated");
     } else {
       const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      const passwordRegex =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-      if (!passwordRegex.test(password)) {
-        document.getElementById("password").classList.add("is-invalid");
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      if (!emailRegex.test(email)) {
+        document.getElementById("email").classList.add("is-invalid");
+        document.getElementById("invalid-feedback-password").style.display =
+          "none";
+        document.getElementById("invalid-feedback-email").textContent =
+          "El email no es válido";
+        document.getElementById("invalid-feedback-email").style.display =
+          "block";
       } else {
-        form.submit();
+        document.getElementById("email").classList.remove("is-invalid");
+        document.getElementById("invalid-feedback-email").style.display =
+          "none";
+        const password = document.getElementById("password").value;
+        const passwordRegex =
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+        if (!passwordRegex.test(password)) {
+          document.getElementById("password").classList.add("is-invalid");
+          document.getElementById("invalid-feedback-password").textContent =
+            "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una minúscula y un número";
+          document.getElementById("invalid-feedback-password").style.display =
+            "block";
+        } else {
+          form.submit();
+          // go to admin page
+          window.location.href = "admin.html";
+        }
       }
     }
   });
