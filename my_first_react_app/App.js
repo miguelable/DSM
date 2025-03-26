b46213d183d3a167d887aa0018de55d37bc6567c
miguelable <miguelff222@gmail.com>
2025-03-26 16:11:54 +0100

    Agregar componente Calendario y datos de excursiones a la aplicación React

--- my_first_react_app/App.js
+++ my_first_react_app/App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import Calendario from "./components/CalendarioComponent";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { EXCURSIONES } from "./common/excursiones";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View>
+        <Calendario excursiones={EXCURSIONES} />
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
