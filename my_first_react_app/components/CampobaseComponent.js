61dd33da3046328653ffb59f6666b5d277fc832d
miguelable <miguelff222@gmail.com>
2025-04-07 18:23:42 +0200

    Agregar componentes de navegación y datos de cabeceras: Practica 5

--- my_first_react_app/components/CampobaseComponent.js
+++ my_first_react_app/components/CampobaseComponent.js
@@ -5,8 +5,11 @@ import DetalleExcursion from "./DetalleExcursionComponent";
 import { Platform, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "./HomeComponent";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function CalendarioNavegador() {
   return (
@@ -37,6 +40,45 @@ function CalendarioNavegador() {
   );
 }
 
+function HomeNavegador() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerMode: "screen",
+        headerTintColor: "#fff",
+        headerStyle: { backgroundColor: "#015afc" },
+        headerTitleStyle: { color: "#fff" },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: "Campo Base",
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function DrawerNavegador() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Campo base"
+      screenOptions={{
+        // headerShown: false,
+        drawerStyle: {
+          backgroundColor: "#c2d3da",
+        },
+      }}
+    >
+      <Drawer.Screen name="Campo base" component={HomeNavegador} />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+    </Drawer.Navigator>
+  );
+}
+
 class Campobase extends Component {
   render() {
     return (
@@ -47,7 +89,7 @@ class Campobase extends Component {
             paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
           }}
         >
-          <CalendarioNavegador />
+          <DrawerNavegador />
         </View>
       </NavigationContainer>
     );
