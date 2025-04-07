b24ba3760e1befa96c89963bcfcc63cdf5ee4e86
miguelable <miguelff222@gmail.com>
2025-04-07 15:34:19 +0200

    Componentes Rect Native - Agregar componente Campobase y DetalleExcursion.

--- my_first_react_app/App.js
+++ my_first_react_app/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { View } from "react-native";
-import Calendario from "./components/CalendarioComponent";
+import Campobase from "./components/CampobaseComponent";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { EXCURSIONES } from "./common/excursiones";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View>
-        <Calendario excursiones={EXCURSIONES} />
+        <Campobase />
       </View>
     </SafeAreaProvider>
   );
---
+++ my_first_react_app/components/CampobaseComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Calendario from "./CalendarioComponent";
+import { EXCURSIONES } from "../common/excursiones";
+class Campobase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      excursiones: EXCURSIONES,
+    };
+  }
+  render() {
+    return <Calendario excursiones={this.state.excursiones} />;
+  }
+}
+export default Campobase;
