26c8e1ff883d22759b7d82b06c0a2da86fa6006c
miguelable <miguelff222@gmail.com>
2025-04-08 23:45:10 +0200

    Actualizar rutas de imágenes en componentes de actividades, cabeceras y excursiones

--- my_first_react_app/common/cabeceras.js
+++ my_first_react_app/common/cabeceras.js
@@ -2,7 +2,7 @@ export const CABECERAS = [
   {
     id: 0,
     nombre: "40 años de historia!",
-    imagen: "imagenes/mendiak.png",
+    imagen: require("../components/imagenes/mendiak.png"),
     destacado: true,
     descripcion:
       "40 años organizando salidas para conocer y disfrutar la montaña",
