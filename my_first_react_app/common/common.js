03a92edc986b22b40a7b3881d4aee68502ed8782
miguelable <miguelff222@gmail.com>
2025-04-15 00:44:25 +0200

    Agregar archivos de configuración y recursos para el servidor json

---
+++ my_first_react_app/common/common.js
@@ -0,0 +1,3 @@
+export const baseUrl = `http://192.168.1.40:3001/`; // URL base del servidor JSON
+export const colorGaztaroa = "rgb(190, 83, 1)";
+export const colorGaztaroaHeader = "rgb(196, 98, 24)"; // Color primario de la aplicación
