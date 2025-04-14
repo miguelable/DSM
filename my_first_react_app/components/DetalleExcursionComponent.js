18c09edff901f4b691c406b99f9a4509986588a8
miguelable <miguelff222@gmail.com>
2025-04-14 15:52:13 +0200

    Agregar componente de comentarios e integrarlo en detalles excursion

--- my_first_react_app/components/DetalleExcursionComponent.js
+++ my_first_react_app/components/DetalleExcursionComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, FlatList } from "react-native";
 import { Card } from "@rneui/themed";
 import { EXCURSIONES } from "../common/excursiones";
+import { COMENTARIOS } from "../common/comentarios";
 
 function RenderExcursion(props) {
   const excursion = props.excursion;
@@ -26,17 +27,58 @@ function RenderExcursion(props) {
   }
 }
 
+function RenderComentario(props) {
+  const comentarios = props.comentarios;
+
+  const renderStars = (valoracion) => {
+    const maxStars = 5;
+    const filledStars = "★".repeat(valoracion);
+    const emptyStars = "☆".repeat(maxStars - valoracion);
+    return filledStars + emptyStars;
+  };
+
+  return (
+    <Card>
+      <Card.Title>Comentarios</Card.Title>
+      <Card.Divider />
+      {comentarios.map((item) => (
+        <View key={item.id} style={{ margin: 10 }}>
+          <Text style={{ fontSize: 16, color: "rgb(190, 83, 1)" }}>
+            {renderStars(item.valoracion)}
+          </Text>
+          <Text style={{ fontSize: 14, fontWeight: "bold", marginTop: 5 }}>
+            {item.autor}
+          </Text>
+          <Text style={{ fontSize: 13, marginTop: 10 }}>{item.comentario}</Text>
+          <Text style={{ fontSize: 12, marginTop: 10 }}>{item.dia}</Text>
+          <Card.Divider style={{ marginTop: 10 }} />
+        </View>
+      ))}
+    </Card>
+  );
+}
+
 class DetalleExcursion extends Component {
   constructor(props) {
     super(props);
     this.state = {
       excursiones: EXCURSIONES,
+      comentarios: COMENTARIOS,
     };
   }
 
   render() {
     const { excursionId } = this.props.route.params;
-    return <RenderExcursion excursion={this.state.excursiones[+excursionId]} />;
+    return (
+      <ScrollView>
+        <RenderExcursion excursion={this.state.excursiones[+excursionId]} />
+        <RenderComentario
+          comentarios={this.state.comentarios.filter(
+            (comentario) => comentario.excursionId === excursionId
+          )}
+        />
+      </ScrollView>
+    );
   }
 }
 
